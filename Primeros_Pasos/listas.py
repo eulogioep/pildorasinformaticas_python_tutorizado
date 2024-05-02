3772707c6017c98ed3514b8f2dfc1bb4286017aa
eulogioep <eulogio-ep@hotmail.com>
2024-05-02 11:00:26 +0200

    listas

---
+++ Primeros_Pasos/listas.py
@@ -0,0 +1,18 @@
+# Crea una lista
+trabajadores = ["Juan", 6, "Ana", "Pedro", "María", True]
+
+# Imprime en consola los elementos de una lista
+print(trabajadores)
+
+# Imprime en consola el primer elemento de una lista
+print(trabajadores[0]) # Accede por el principio de la lista
+print(trabajadores[-1]) # Accede por el final de la lista
+
+# Agregar un elemento a una lista en la última posición
+trabajadores.append("Luis")
+
+# Indica el tamaño de una lista
+print(len(trabajadores))
+
+
+
