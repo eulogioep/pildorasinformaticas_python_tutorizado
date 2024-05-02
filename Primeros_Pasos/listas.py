c03981954abbf86b08a7b115753639dd512dbef6
eulogioep <eulogio-ep@hotmail.com>
2024-05-02 14:49:02 +0200

    Listas

--- Primeros_Pasos/listas.py
+++ Primeros_Pasos/listas.py
@@ -7,12 +7,23 @@ print(trabajadores)
 # Imprime en consola el primer elemento de una lista
 print(trabajadores[0]) # Accede por el principio de la lista
 print(trabajadores[-1]) # Accede por el final de la lista
+print(trabajadores[0:3]) # Accede desde el primer elemento hasta el tercero sin incluirlo
 
-# Agregar un elemento a una lista en la última posición
+# Agrega un elemento al final de una lista.
+# Uso común: Cuando deseas agregar un solo elemento a una lista existente.
 trabajadores.append("Luis")
+print(trabajadores)
+
+# Agrega varios elementos al final de una lista.
+# Uso común: Cuando deseas agregar múltiples elementos desde otra lista o secuencia.
+trabajadores.extend([3,5])
+print(trabajadores)
+
+# Elimina la primera aparición de un elemento con el valor especificado en una lista.
+# Modifica la lista original al eliminar el elemento. Si el elemento aparece más de una vez 
+# en la lista, solo se eliminará la primera aparición.
+trabajadores.remove("Pedro")
+
 
 # Indica el tamaño de una lista
 print(len(trabajadores))
-
-
-
